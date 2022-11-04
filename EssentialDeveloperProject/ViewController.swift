0c691473e1d491922fe3a67834547102968868da
Fernando Monteiro <fernando.monteiro@mobile2you.com.br>
2022-11-04 17:14:55 -0300

    Initial Commit

---
+++ EssentialDeveloperProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialDeveloperProject
+//
+//  Created by Fernando Monteiro on 04/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
