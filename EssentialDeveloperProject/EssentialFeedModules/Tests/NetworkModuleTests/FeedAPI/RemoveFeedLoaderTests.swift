65f8eb2127daca9ae2482263995e040abd012b4f
Fernando Monteiro <fernando.vicente@dimensa.com.br>
2023-01-19 17:55:13 -0300

    Network_07: Remove LoadFeedResult equatable conformance

--- EssentialDeveloperProject/EssentialFeedModules/Tests/NetworkModuleTests/FeedAPI/RemoveFeedLoaderTests.swift
+++ EssentialDeveloperProject/EssentialFeedModules/Tests/NetworkModuleTests/FeedAPI/RemoveFeedLoaderTests.swift
@@ -135,17 +135,29 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private func expect(
         _ sut: RemoteFeedLoader,
-        toCompleteWithResult result: RemoteFeedLoader.Result,
+        toCompleteWithResult expectedResult: RemoteFeedLoader.Result,
         when action: () -> Void,
         file: StaticString = #filePath,
         line: UInt = #line
     ) {
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError)
+            default:
+                XCTFail("Expected result \(expectedResult) but got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
         
         action()
         
-        XCTAssertEqual(capturedResults, [result], file: file, line: line)
+        waitForExpectations(timeout: 1.0)
     }
     
     private func makeSUT(
