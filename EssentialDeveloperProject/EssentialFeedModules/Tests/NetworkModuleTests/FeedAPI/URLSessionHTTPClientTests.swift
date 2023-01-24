15ba3caf44cbda3a30b49d85ea628a099b498f33
Fernando Monteiro <fernando.vicente@dimensa.com.br>
2023-01-24 13:58:25 -0300

    Network_08: Resumes data task on get from url

--- EssentialDeveloperProject/EssentialFeedModules/Tests/NetworkModuleTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialDeveloperProject/EssentialFeedModules/Tests/NetworkModuleTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -15,7 +15,9 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in }
+        // This implementation should be private, we should test
+        // only the behavior, not the implementation details
+        session.dataTask(with: url) { _, _, _ in }.resume()
     }
 }
 
@@ -31,19 +33,44 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedURLs, [url])
     }
     
-    // We are subclassing URLSession. But don't have access to the internal
-    // implementation. Plus this types contain many methods we won't use
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
+
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+    
     private class URLSessionSpy: URLSession {
         var receivedURLs = [URL]()
+        private var stubs = [URL: URLSessionDataTask]()
         
-        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            // Capturing URL when dataTask() is called
-            receivedURLs.append(url)
-            
-            // We don't want a request to be executed, so we return a fake DataTask
-            return FakeURLSessionDataTask()
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
         }
         
-        private class FakeURLSessionDataTask: URLSessionDataTask {}
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            
+            return stubs[url] ?? FakeURLSessionDataTask()
+        }
+        
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() {}
+    }
+    
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCallCount = 0
+        
+        override func resume() {
+            resumeCallCount += 1
+        }
     }
 }
