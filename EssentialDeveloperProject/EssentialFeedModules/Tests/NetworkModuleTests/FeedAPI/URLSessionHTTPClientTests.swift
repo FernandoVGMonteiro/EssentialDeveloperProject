a4c6ceffe06dce6e63832b394f1f30d3e06cb74f
Fernando Monteiro <fernando.vicente@dimensa.com.br>
2023-02-02 12:24:21 -0300

    Network_09: Added factory method to create SUT. Moved start and stop intercepting requests to setUp and tearDown methods

--- EssentialDeveloperProject/EssentialFeedModules/Tests/NetworkModuleTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialDeveloperProject/EssentialFeedModules/Tests/NetworkModuleTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -25,9 +25,19 @@ class URLSessionHTTPClient {
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
-    func test_getFromURL_performsGETRequestWithURL() {
+    
+    override func setUp() {
+        super.setUp()
         URLProtocolStub.startInterceptingRequests()
         
+    }
+    
+    override func tearDown() {
+        URLProtocolStub.stopInterceptingRequests()
+        super.tearDown()
+    }
+    
+    func test_getFromURL_performsGETRequestWithURL() {
         let url = URL(string: "http://any-url.com")!
         let exp = expectation(description: "Wait for completion")
 
@@ -37,25 +47,19 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         waitForExpectations(timeout: 1)
-        
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     func test_getFromURL_failsOnRequestError() {
-        URLProtocolStub.startInterceptingRequests()
-        
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
-        let sut = URLSessionHTTPClient()
-        
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -68,8 +72,12 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         waitForExpectations(timeout: 1)
-        
-        URLProtocolStub.stopInterceptingRequests()
+    }
+    
+    // This protects the test from breaking changes
+    // Eg.: if we need to pass a dependency to the client
+    private func makeSUT() -> URLSessionHTTPClient {
+        URLSessionHTTPClient()
     }
     
     private class URLProtocolStub: URLProtocol {
