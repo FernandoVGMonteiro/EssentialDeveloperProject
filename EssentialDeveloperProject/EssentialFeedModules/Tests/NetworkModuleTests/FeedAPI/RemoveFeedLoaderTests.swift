974f721be93781776e417536ed24b5701f6c06a3
Fernando Monteiro <fernando.vicente@dimensa.com.br>
2023-02-02 12:28:52 -0300

    Network_09: Extract memory leak tracker to a helper extension

--- EssentialDeveloperProject/EssentialFeedModules/Tests/NetworkModuleTests/FeedAPI/RemoveFeedLoaderTests.swift
+++ EssentialDeveloperProject/EssentialFeedModules/Tests/NetworkModuleTests/FeedAPI/RemoveFeedLoaderTests.swift
@@ -178,12 +178,6 @@ class RemoteFeedLoaderTests: XCTestCase {
         return (sut, client)
     }
     
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been dealocated. Potential memory leak.", file: file, line: line)
-        }
-    }
-    
     private class HTTPClientSpy: HTTPClient {
         
         private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
